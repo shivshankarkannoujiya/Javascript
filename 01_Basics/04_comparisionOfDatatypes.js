b852252ee57d13c9453ac327fa8f5f5430d76f09
shivshankarkannoujiya <kannoujiyaji143@gmail.com>
2023-12-16 00:46:55 +0530

    datatypeComparision

---
+++ 01_Basics/04_comparisionOfDatatypes.js
@@ -0,0 +1,23 @@
+// console.log(2 >= 1);
+// console.log(2 > 1);
+// console.log(2 == 1);
+// console.log(2 < 1);
+// console.log(2 <= 1);
+// console.log(2 != 1);
+
+// with diff dataTypes
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null <= 0);
+// console.log(null >= 0);
+
+// console.log(undefined == 0);
+// console.log(undefined < 0);
+// console.log(undefined > 0);
+
+// strict-check === check --> value+Datatype
+console.log("2" == 2);
+console.log("2" === 2);
