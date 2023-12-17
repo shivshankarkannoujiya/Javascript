2a751972d372f45df1f9ac1a7e90b61e9605112c
shivshankarkannoujiya <kannoujiyaji143@gmail.com>
2023-12-17 22:47:16 +0530

    if-else

---
+++ 04_controlFlow/one.js
@@ -0,0 +1,43 @@
+// if
+
+// const isUserLoggedIn = true;
+// const temperature = 41;
+// if (temperature < 50) {
+//   console.log("less than 50");
+// } else {
+//   console.log("temperature is greater than 50");
+// }
+
+// const score = 200;
+// if (score > 100) {
+//   const power = "fly";
+//   console.log(`User power : ${power}`);
+// }
+// console.log(`User power : ${power}`);
+
+// shorthand notation
+// const balance = 1000;
+// if (balance > 500) console.log("test"),console.log("test2");
+
+// if (balance < 500) {
+//   console.log("less than 500");
+// } else if (balance < 750) {
+//   console.log("less than 750");
+// } else if (balance < 900) {
+//   console.log("leass than 900");
+// } else {
+//   console.log("less than 1200");
+// }
+
+const UserLoggedIn = true;
+const debitCard = true;
+const loggedInFromGoogle = false;
+const loggedInFromEmail = true;
+
+if (UserLoggedIn && debitCard && 2 == 2) {
+  console.log("Allow to Buy Courses");
+}
+
+if (loggedInFromGoogle || loggedInFromEmail) {
+  console.log("User logged in");
+}
