91f6489b0b84465814ea1d4b06bb6dad6332141b
shivshankarkannoujiya <kannoujiyaji143@gmail.com>
2023-12-22 01:21:13 +0530

    Object

---
+++ ExtraClasses/01_object.js
@@ -0,0 +1,12 @@
+// object creation
+const rectangle = {
+  length: 1,
+  breadth: 2,
+
+  draw: function () {
+    console.log("Drawing rectangle...");
+  },
+};
+console.log(rectangle);
+console.log(rectangle.draw());
+console.log(rectangle.length);
---
+++ ExtraClasses/Nots.md
@@ -0,0 +1,20 @@
+#Object
+
+##creating object
+
+```javascript
+
+const rectangle = {
+  // property
+  length: 1,
+  breadth: 2,
+
+  // Behaviour:method
+  draw: function () {
+    console.log("Draw");
+  },
+};
+console.log(rectangle);
+
+```
+
